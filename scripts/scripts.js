1ae10776c43999f3f4664b9a5b285fa03fff4e67
Zihan Wang <jsurmacz@bu.edu>
2023-12-05 14:09:09 -0500

    change var

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,15 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
-function redirectToCreatage() {
-    window.location.href = 'create.html';
-}
-
-// scripts.js
-
-// scripts.js
 
 function redirectToCreatePage() {
   // Create a new big-circle element
-  var newBigCircle = document.createElement("div");
+  const newBigCircle = document.createElement("div");
   newBigCircle.className = "big-circle";
 
   // Generate random size and position within the flex container
-  var flexContainer = document.querySelector("main > div");
-  var randomSize = Math.floor(Math.random() * 100) + 50; // Adjust the range of sizes as needed
-  var randomLeft = Math.floor(Math.random() * (flexContainer.clientWidth - randomSize));
-  var randomTop = Math.floor(Math.random() * (flexContainer.clientHeight - randomSize));
+  const flexContainer = document.querySelector("main > div");
+  const randomSize = Math.floor(Math.random() * 100) + 50;
+  let randomLeft = Math.floor(Math.random() * (flexContainer.clientWidth - randomSize));
+  let randomTop = Math.floor(Math.random() * (flexContainer.clientHeight - randomSize));
 
   // Set the style for the new big-circle element
   newBigCircle.style.width = randomSize + "px";
@@ -37,9 +30,9 @@ function redirectToCreatePage() {
 }
 
 function checkOverlap(newCircle) {
-  var existingCircles = document.querySelectorAll(".big-circle");
-  for (var i = 0; i < existingCircles.length; i++) {
-    var existingCircle = existingCircles[i];
+  const existingCircles = document.querySelectorAll(".big-circle");
+  for (let i = 0; i < existingCircles.length; i++) {
+    const existingCircle = existingCircles[i];
     if (isOverlap(newCircle, existingCircle)) {
       return true; // Overlapping found
     }
@@ -48,8 +41,8 @@ function checkOverlap(newCircle) {
 }
 
 function isOverlap(circle1, circle2) {
-  var rect1 = circle1.getBoundingClientRect();
-  var rect2 = circle2.getBoundingClientRect();
+  const rect1 = circle1.getBoundingClientRect();
+  const rect2 = circle2.getBoundingClientRect();
   return !(
     rect1.right < rect2.left ||
     rect1.left > rect2.right ||
